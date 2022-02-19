56ca5bd45f55e06f1fd2eddca46a933feca4aa8d
harish <73505107+harishselvakumar05@users.noreply.github.com>
2022-02-19 15:04:06 -0800

    Update CessnaGUIPlugin.hh

--- plugins/CessnaGUIPlugin.hh
+++ plugins/CessnaGUIPlugin.hh
@@ -14,143 +14,113 @@
  * limitations under the License.
  *
 */
+#ifndef GAZEBO_PLUGINS_CESSNAGUIPLUGIN_HH_
+#define GAZEBO_PLUGINS_CESSNAGUIPLUGIN_HH_
 
-#ifndef GAZEBO_PLUGINS_CESSNAPLUGIN_HH_
-#define GAZEBO_PLUGINS_CESSNAPLUGIN_HH_
-
-#include <array>
 #include <mutex>
-#include <string>
-#include <sdf/sdf.hh>
-#include <ignition/transport/Node.hh>
-#include <gazebo/common/PID.hh>
+
+#include <ignition/math/Angle.hh>
+
 #include <gazebo/common/Plugin.hh>
-#include <gazebo/common/UpdateInfo.hh>
-#include <gazebo/msgs/msgs.hh>
-#include <gazebo/physics/PhysicsTypes.hh>
-#include <gazebo/transport/TransportTypes.hh>
+#include <gazebo/gui/GuiPlugin.hh>
+// moc parsing error of tbb headers
+#ifndef Q_MOC_RUN
+#include <gazebo/transport/transport.hh>
+#endif
 
 namespace gazebo
 {
-  /// \brief Allow moving the control surfaces of a Cessna C-172 plane. This
-  /// plugin might be used with other models that have similar control surfaces.
+  /// \brief A GUI plugin that controls the Cessna model using the keyboard.
+  /// If you are reading this, feel free to improve this plugin by adding
+  /// graphical widgets to make the demo more interesting and fun.
   ///
-  /// The plugin requires the following parameters:
-  /// <propeller>         Name of the joint controlling the propeller spin.
-  /// <propeller_max_rpm> Maximum angular speed in rpm.
-  /// <left_aileron>      Name of the joint controlling the left aileron.
-  /// <left_flap>         Name of the joint controlling the left flap.
-  /// <right_aileron>     Name of the joint controlling the right aileron.
-  /// <right_flap>        Name of the joint controlling the right flap.
-  /// <elevators>         Name of the joint controlling the rear elevators.
-  /// <rudder>            Name of the joint controlling the rudder.
-  ///
-  /// The following parameters are optional:
-  /// <propeller_p_gain> P gain for the PID that controls the propeller's speed.
-  /// <propeller_i_gain> I gain for the PID that controls the propeller's speed.
-  /// <propeller_d_gain> D gain for the PID that controls the propeller's speed.
-  /// <surfaces_p_gain> P gain for the PID that controls the position of the
-  ///                   control surfaces.
-  /// <surfaces_i_gain> I gain for the PID that controls the position of the
-  ///                   control surfaces.
-  /// <surfaces_d_gain> D gain for the PID that controls the position of the
-  ///                   control surfaces.
-  ///
-  /// The plugin will be subscribed to the following topic:
-  /// "~/<model_name>/control" The expected value is a Cessna message.
-  ///
-  /// The plugin will advertise the following topic with the current state:
-  /// "~/<model_name>/state"
-  class GZ_PLUGIN_VISIBLE CessnaPlugin : public ModelPlugin
+  /// Keyboard controls:
+  /// w         Increase thrust (+10 %)
+  /// s         Decrease thrust (-10 %)
+  /// d         Increase rudder angle (+1 degree)
+  /// a         Decrease rudder angle (-1 degree)
+  /// Left-Key  Left roll (+1 degree)
+  /// Right-Key Right roll (+1 degree)
+  /// Up-Key    Pitch down (+1 degree)
+  /// Down-Key  Pitch up (+1 degree)
+  /// 1         Preset for take-off
+  /// 2         Preset for cruise
+  /// 3         Preset for landing
+  class GZ_PLUGIN_VISIBLE CessnaGUIPlugin : public GUIPlugin
   {
+    Q_OBJECT
+
     /// \brief Constructor.
-    public: CessnaPlugin();
+    public: CessnaGUIPlugin();
 
     /// \brief Destructor.
-    public: ~CessnaPlugin();
+    public: virtual ~CessnaGUIPlugin();
 
-    // Documentation Inherited.
-    public: virtual void Load(physics::ModelPtr _model, sdf::ElementPtr _sdf);
+    /// \brief Callback that receives a control message from
+    /// the ~/cessna_c172/state topic.
+    /// \param[in] _msg State msg.
+    private: void OnState(ConstCessnaPtr &_msg);
 
-    /// \brief Read an SDF parameter with a joint name and initialize a pointer
-    /// to this joint.
-    /// \param[in] _sdfParam SDF parameter containing a joint name.
-    /// \param[in] _sdf Pointer to the SDF element containing the parameters.
-    /// \param[out] _joint Pointer to the joint to be initialized.
-    /// \return True if the SDF parameter is found and the joint name is found,
-    ///         false otherwise.
-    private: bool FindJoint(const std::string &_sdfParam,
-        sdf::ElementPtr _sdf, physics::JointPtr &_joint);
+    /// \brief Increase the propeller RPMs.
+    private slots: void OnIncreaseThrust();
 
-    /// \brief Update the control surfaces controllers.
-    /// \param[in] _info Update information provided by the server.
-    private: void Update(const common::UpdateInfo &_info);
+    /// \brief Decrease the propeller RPMs.
+    private slots: void OnDecreaseThrust();
 
-    /// \brief Callback executed when a new message containing control commands
-    /// is received.
-    /// \param[in] _msg New message containing control commands.
-    private: void OnControl(ConstCessnaPtr &_msg);
+    /// \brief Increase the flaps angle.
+    private slots: void OnIncreaseFlaps();
 
-    /// \brief Update PID Joint controllers.
-    /// \param[in] _dt time step size since last update.
-    private: void UpdatePIDs(double _dt);
+    /// \brief Decrease the flaps angle.
+    private slots: void OnDecreaseFlaps();
 
-    /// \brief Publish Cessna state.
-    private: void PublishState();
+    /// \brief Increase Roll.
+    private slots: void OnIncreaseRoll();
 
-    /// \brief Joint indexes.
-    private: static const unsigned int kLeftAileron  = 0;
-    private: static const unsigned int kLeftFlap     = 1;
-    private: static const unsigned int kRightAileron = 2;
-    private: static const unsigned int kRightFlap    = 3;
-    private: static const unsigned int kElevators    = 4;
-    private: static const unsigned int kRudder       = 5;
-    private: static const unsigned int kPropeller    = 6;
+    /// \brief Decrease Roll.
+    private slots: void OnDecreaseRoll();
 
-    /// \brief Pointer to the update event connection.
-    private: event::ConnectionPtr updateConnection;
+    /// \brief Increase the elevators angle.
+    private slots: void OnIncreaseElevators();
 
-    /// \brief Node used for using Gazebo communications.
-    private: transport::NodePtr node;
+    /// \brief Decrease the elevators angle.
+    private slots: void OnDecreaseElevators();
 
-    /// \brief Subscriber pointer.
-    private: transport::SubscriberPtr controlSub;
+    /// \brief Increase the rudder angle.
+    private slots: void OnIncreaseRudder();
 
-    /// \brief Publisher pointer.
-    private: transport::PublisherPtr statePub;
+    /// \brief Decrease the rudder angle.
+    private slots: void OnDecreaseRudder();
 
-    /// \brief Pointer to the model;
-    private: physics::ModelPtr model;
+    /// \brief Take-off preset.
+    private slots: void OnPresetTakeOff();
 
-    /// \brief Control surfaces joints.
-    private: std::array<physics::JointPtr, 7> joints;
+    /// \brief Cruise preset.
+    private slots: void OnPresetCruise();
 
-    /// \brief Max propeller RPM.
-    private: int32_t propellerMaxRpm = 2500;
+    /// \brief Landing preset.
+    private slots: void OnPresetLanding();
 
-    /// \brief Next command to be applied to the propeller and control surfaces.
-    private: std::array<float, 7> cmds;
+    /// \brief SDF for this plugin.
+    private: sdf::ElementPtr sdf;
 
-    /// \brief Velocity PID for the propeller.
-    private: common::PID propellerPID;
+    /// \brief Pointer to a node for communication.
+    private: transport::NodePtr gzNode;
 
-    /// \brief Position PID for the control surfaces.
-    private: std::array<common::PID, 6> controlSurfacesPID;
+    /// \brief Control publisher.
+    private: transport::PublisherPtr controlPub;
 
-    /// \brief keep track of controller update sim-time.
-    private: gazebo::common::Time lastControllerUpdateTime;
+    /// \brief State subscriber.
+    private: transport::SubscriberPtr stateSub;
 
-    /// \brief Controller update mutex.
+    /// \brief Angle increment/decrement each time a key is pressed;
+    private: ignition::math::Angle angleStep;
+
+    /// \brief State received from the Cessna plugin.
+    private: msgs::Cessna state;
+
+    /// \brief Protection.
     private: std::mutex mutex;
-
-    // Place ignition::transport objects at the end of this file to
-    // guarantee they are destructed first.
-
-    /// \brief Ignition node used for using Gazebo communications.
-    private: ignition::transport::Node nodeIgn;
-
-    /// \brief Ignition Publisher.
-    private: ignition::transport::Node::Publisher statePubIgn;
   };
 }
+
 #endif
