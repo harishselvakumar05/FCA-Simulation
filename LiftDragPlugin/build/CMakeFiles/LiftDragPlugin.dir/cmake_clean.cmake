0d18d44748373b4c3d9e0c866f7d8cba823e4cce
harish <73505107+harishselvakumar05@users.noreply.github.com>
2022-03-13 16:05:07 -0700

    Add files via upload

---
+++ LiftDragPlugin/build/CMakeFiles/LiftDragPlugin.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LiftDragPlugin.dir/LiftDragPlugin.cc.o"
+  "libLiftDragPlugin.pdb"
+  "libLiftDragPlugin.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LiftDragPlugin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
